27619b51704b75dbb562e1c7369fabd7e72ea9b9
Alexandru Robert <alexandru.robert@babelgroup.com>
2022-03-01 21:58:26 +0100

    First swiftui app
    Created an simple swift UI app and used few kind of async image mode from url, and animations.

---
+++ FirstSwiftUIApp/FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by alexandru.robert.local on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
